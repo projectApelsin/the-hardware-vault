ad0d28b5384d09032b18baf8c078c430141ba194
projectApelsin <copies.everywhere@gmail.com>
2025-02-15 16:52:33 +0200

    refactoring

---
+++ server/the-hardware-vault/src/main/java/com/e_commerce/the_hardware_vault/model/repository/CharacteristicRepository.java
@@ -0,0 +1,4 @@
+package com.e_commerce.the_hardware_vault.model.repository;
+
+public interface CharacteristicRepository extends org.springframework.data.jpa.repository.JpaRepository<com.e_commerce.the_hardware_vault.model.Characteristic, java.lang.Integer> {
+  }
