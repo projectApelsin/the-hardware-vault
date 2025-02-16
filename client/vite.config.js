583ba565fffc71f4568480319a3af931ffbffa23
projectApelsin <copies.everywhere@gmail.com>
2025-02-16 20:33:30 +0200

    refactoring

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,16 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://backend-production-e7c9.up.railway.app',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-    
-}
+ 
 })
