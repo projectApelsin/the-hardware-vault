8c73d497e6864e348eb5bd52442f136693a0feaf
projectApelsin <copies.everywhere@gmail.com>
2025-02-16 20:39:44 +0200

    refactoring

--- server/the-hardware-vault/src/main/java/com/e_commerce/the_hardware_vault/api/security/SecurityConfig.java
+++ server/the-hardware-vault/src/main/java/com/e_commerce/the_hardware_vault/api/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://frontend-production-d351.up.railway.app"));
+        configuration.setAllowedOrigins(List.of("https://thehardwarevault.up.railway.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
